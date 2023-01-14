51a44736d7825a47c8063676eb3a4d3add52cad0
Mirza Ramadhani <ramadhanimirza10@gmail.com>
2023-01-14 09:35:24 +0700

    Update main.go

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func main()  {
 	router.PUT("/persons/:id", controllers.UpdatePerson)
 	router.DELETE("/persons/:id", controllers.DeletePerson)
 
-	router.Run(":"+os.Getenv("PGPORT"))
+	router.Run(":"+os.Getenv("PORT"))
 
 }
 
